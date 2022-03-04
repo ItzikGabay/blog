00b60ceb2febad0c29cba7bff96ca5fc29f49b7d
ItzikDevio <itzikdevio@gmail.com>
2022-03-05 00:39:31 +0200

    Added navbar design

--- layout/header/navbar/navbar.tsx
+++ layout/header/navbar/navbar.tsx
@@ -1,5 +1,19 @@
 import styles from "./navbar.module.css";
 
 export const Navbar = () => {
-  return <div className={styles.nav__container}>navbar 50px</div>;
+  return (
+    <div className={styles.nav__container}>
+      <div className={styles.nav__left}>
+        <div className={styles.nav__left_logo}>Itzik Gabay</div>
+        <div className={styles.nav__left_links}>
+          <div className={styles.nav__left_link}>All Posts</div>
+          <div className={styles.nav__left_link}>Snippets</div>
+          <div className={styles.nav__left_link}>Buy me a coffe</div>
+        </div>
+      </div>
+      <div className={styles.nav__right}>
+        <div className={styles.nav__right_buttons}>right</div>
+      </div>
+    </div>
+  );
 };
