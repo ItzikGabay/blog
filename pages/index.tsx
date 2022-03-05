9fb7dd7b6e6ceffe24790c5e02e64e6bb67efa05
ItzikDevio <itzikdevio@gmail.com>
2022-03-05 20:02:44 +0200

    added getstaticprops function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
-import type { NextPage } from "next";
+import React, { useEffect } from "react";
+import type { GetStaticPropsContext, GetStaticPropsResult, NextPage } from "next";
 import styles from "../styles/pages/home.module.css";
 
 import { ThemeSection } from "../components/UI/theme-section/theme-section";
 import ThemeSectionTitle from "../components/UI/theme-section-title/theme-section-title";
 import PostList from "../components/post-list/post-list";
 import Sidebar from "../components/sidebar/sidebar";
+import { Params } from "next/dist/server/router";
 
-const Home: NextPage = () => {
+interface IIndexPageProps {
+  posts: String | Array<Object>;
+}
+
+const Home: NextPage<IIndexPageProps> = ({ posts }) => {
+  console.log(posts);
+
   return (
     <ThemeSection>
       <div className={styles.home__container}>
@@ -23,3 +31,11 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export function getStaticProps(context: GetStaticPropsContext): GetStaticPropsResult<Params> {
+  return {
+    props: {
+      posts: "This is post",
+    },
+  };
+}
---
+++ services/mdx/posts/01-first-post.md
@@ -0,0 +1,18 @@
+---
+title: "First post"
+date: "Januar 01 2022"
+excerpt: "This is excerpt"
+cover_image: "../../../assets/posts/01-first-posts.png"
+---
+
+## Hello world
+
+### IF YOU SEE THAT, this is working!
+
+What do you think about it?
+
+It is look nice or not?
+
+- One
+- Two
+- Three
