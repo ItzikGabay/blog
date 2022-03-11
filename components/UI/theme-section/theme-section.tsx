c5acb8048ddf14e454ed50102106ad14b6616fda
ItzikDevio <itzikdevio@gmail.com>
2022-03-12 01:08:05 +0200

    Added some css tweaks

--- components/UI/theme-section/theme-section.tsx
+++ components/UI/theme-section/theme-section.tsx
@@ -1,10 +1,19 @@
 import React, { ReactChildren, ReactChild } from "react";
 import styles from "./theme-section.module.css";
+import { useRouter } from "next/router";
 
 interface ThemeSectionProps {
   children: ReactChild | ReactChildren;
 }
 
 export const ThemeSection = ({ children }: ThemeSectionProps) => {
-  return <div className={styles.section__container}>{children}</div>;
+  const router = useRouter();
+
+  return (
+    <div
+      className={styles.section__container}
+      style={{ maxWidth: router.pathname === "/" ? "1300px" : "900px" }}>
+      {children}
+    </div>
+  );
 };
--- layout/header/header.tsx
+++ layout/header/header.tsx
@@ -2,15 +2,20 @@ import styles from "./header.module.css";
 import { Navbar } from "./components/navbar/navbar";
 import Image from "next/image";
 import wave from "../../assets/illustrations/wave.svg";
+import { useRouter } from "next/router";
 
 export const Header = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.header__container}>
       <div className={styles.header__content}>
         <Navbar />
       </div>
-      <div className={styles.header__img}>
-        <Image src={wave} height={200} width={900} />
+      <div
+        className={styles.header__img}
+        style={{ height: router.pathname !== "/" ? "100px" : undefined }}>
+        <Image src={wave} height={200} width={900} alt="navbar-image" />
       </div>
     </div>
   );
